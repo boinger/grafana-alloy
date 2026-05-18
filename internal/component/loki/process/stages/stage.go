grafana/alloy

fix(loki.process): Return errors from metric registration instead of panicking
Two functions in the pipeline stages package (getDropCountMetric and
registerCounterVec) called panic(err) when Prometheus metric
registration failed for non-duplicate reasons. This could crash
the entire Alloy process on an otherwise recoverable error.

Change both functions to return errors, and propagate them through
all six callers (match, drop, sampling, pack, limit, pipeline) and
the stage factory. Two stage constructors (newSamplingStage,
newPackStage) that previously returned only Stage now return
(Stage, error) to support error propagation.

Resolves the TODO at match.go:107 added in #5399.

internal/component/loki/process/stages/stage.go
@@ -149,7 +149,10 @@ func New(logger log.Logger, cfg StageConfig, registerer prometheus.Registerer, m
 			return nil, err
 		}
 	case cfg.PackConfig != nil:
-		s = newPackStage(logger, *cfg.PackConfig, registerer)
+		s, err = newPackStage(logger, *cfg.PackConfig, registerer)
+		if err != nil {
+			return nil, err
+		}
 	case cfg.LabelAllowConfig != nil:
 		s, err = newLabelAllowStage(*cfg.LabelAllowConfig)
 		if err != nil {
@@ -176,7 +179,10 @@ func New(logger log.Logger, cfg StageConfig, registerer prometheus.Registerer, m
 			return nil, err
 		}
 	case cfg.SamplingConfig != nil:
-		s = newSamplingStage(logger, *cfg.SamplingConfig, registerer)
+		s, err = newSamplingStage(logger, *cfg.SamplingConfig, registerer)
+		if err != nil {
+			return nil, err
+		}
 	case cfg.EventLogMessageConfig != nil:
 		s = newEventLogMessageStage(logger, cfg.EventLogMessageConfig)
 	case cfg.WindowsEventConfig != nil:

boinger/grafana-alloy

internal/component/loki/process/stages/stage.go
@@ -156,7 +156,10 @@ func New(slogger *slog.Logger, cfg StageConfig, registerer prometheus.Registerer
 			return nil, err
 		}
 	case cfg.PackConfig != nil:
-		s = newPackStage(slogger, *cfg.PackConfig, registerer)
+		s, err = newPackStage(slogger, *cfg.PackConfig, registerer)
+		if err != nil {
+			return nil, err
+		}
 	case cfg.LabelAllowConfig != nil:
 		s, err = newLabelAllowStage(*cfg.LabelAllowConfig)
 		if err != nil {
@@ -183,7 +186,10 @@ func New(slogger *slog.Logger, cfg StageConfig, registerer prometheus.Registerer
 			return nil, err
 		}
 	case cfg.SamplingConfig != nil:
-		s = newSamplingStage(slogger, *cfg.SamplingConfig, registerer)
+		s, err = newSamplingStage(slogger, *cfg.SamplingConfig, registerer)
+		if err != nil {
+			return nil, err
+		}
 	case cfg.EventLogMessageConfig != nil:
 		s = newEventLogMessageStage(slogger, cfg.EventLogMessageConfig)
 	case cfg.WindowsEventConfig != nil: